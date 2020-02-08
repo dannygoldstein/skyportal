1577b4f379da83074a4c91f4a578a7f2e6f9e128
Danny Goldstein <dgold@berkeley.edu>
2020-02-08 14:43:59 -0800

    fix next reference

--- static/js/components/Source.jsx
+++ static/js/components/Source.jsx
@@ -118,7 +118,7 @@ const Source = ({ route }) => {
               type="checkbox"
               id="highPriorityCheckbox"
               checked={Boolean(source.score)}
-              onClick={(event) => updateScore(source.id, +event.target.checked)}
+              onClick={(event) => Action.updateScore(source.id, +event.target.checked)}
             />
             &nbsp;High Priority
           </div>
